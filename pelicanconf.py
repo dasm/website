8c68a583991e4673cafc07e214b9a7a2a1a184c6
Dariusz Smigiel <1412617+dasm@users.noreply.github.com>
2020-09-15 16:53:20 -0700

    Added link to github

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'dasm'
-SITENAME = 'smigiel.dev'
+SITENAME = 'blog.smigiel.dev'
 SITEURL = ''
 
 PATH = 'content'
@@ -13,19 +13,10 @@ TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = 'en'
 
 # all defaults to True.
-DISPLAY_HEADER = True
-DISPLAY_FOOTER = True
-DISPLAY_HOME   = True
-DISPLAY_MENU   = True
-
-# Feed generation is usually not desired when developing
-#FEED_ALL_ATOM = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#DISPLAY_HEADER = True
+#DISPLAY_FOOTER = True
+#DISPLAY_HOME   = True
+#DISPLAY_MENU   = True
 
 ARCHIVES_URL       = 'archives'
 ARCHIVES_SAVE_AS   = 'archives.html'
@@ -35,15 +26,16 @@ MENU_INTERNAL_PAGES = (
 )
 
 # additional menu items
-#MENUITEMS = (
-#    ('GitHub', 'https://github.com/'),
-#)
+MENUITEMS = (
+    ('GitHub', 'https://github.com/dasm'),
+)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-#DEFAULT_PAGINATION = True
+SUMMARY_MAX_LENGTH = 50
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
